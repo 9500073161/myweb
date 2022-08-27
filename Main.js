07a97de9263c8de728f2a033d7d4f98571549021
prashob <vp.prashob@gmail.com>
2022-08-27 11:03:02 +0200

    .\Main.js

---
+++ Main.js
@@ -0,0 +1,11 @@
+  let name1 = 'Prashob';
+  let name2 = 'Vikram';
+  let name3 = 'Rohit';
+  let name4 = 'Souvik';
+  let result = 'Prashob,Vikram,Rohit,Souvik';
+
+  let heading = document.querySelector('.heading')
+  // show the result in web page
+  heading.innerText = `TCS OC team ${result}`
+
+  console.log(`TCS OC Team ${result}`);
